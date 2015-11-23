337bbe0288d8af69b9a2c2645bcbf88dcef5a86b
Sergii Katrych <sergii.katrych@westwing.de>
2015-11-23 13:10:35 +0100

    Add general purpose Simple adapter class to communicate with vertica

---
+++ src/Adapter/Wrapper/Simple.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * General purpose adapter that provides communication
+ * layer to HP Vertica db for your PHP application.
+ *
+ * @author Sergii Katrych <sergii.katrych@westwing.de>
+ * @date 23/11/15
+ */
+
+namespace VerticaPhpAdapter\Adapter\Wrapper;
+
+
+use VerticaPhpAdapter\Adapter\Odbc\VerticaOdbcAbstract;
+
+class Simple extends VerticaOdbcAbstract
+{
+
+}
